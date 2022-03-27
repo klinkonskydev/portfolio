694396f80ab08a29e68937681dc0a8661859bd80
klinkonskydev <klinkonskydev@gmail.com>
2022-03-27 14:43:26 -0300

    fix - removed cardList component

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,14 +1,12 @@
 import { Grid } from 'components/Grid'
 import { Container } from 'components/Container'
 import Hero from 'components/Hero'
-import CardList from 'components/CardList'
 
 export default function HomeTemplate() {
   return (
     <Container>
       <Grid>
         <Hero />
-        <CardList item={[]} />
       </Grid>
     </Container>
   )
